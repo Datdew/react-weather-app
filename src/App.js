cc2b1d84a4a279207f52046dce26449e80d6f3f2
Datdew <datdew62@gmail.com>
2025-03-09 02:54:21 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by Datali-Dew and its {""}
-        <a
-          className="GitHub-link"
-          href="https://github.com/Datdew/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        
+        <footer>
+          Coded by Datali-Dew and its {""}
+          <a
+            className="GitHub-link"
+            href="https://github.com/Datdew/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

962abdee9a0306e4b24da24953e70e3e0ac244d4
Datdew <datdew62@gmail.com>
2025-03-17 18:38:07 +0100

    added styling for better visuals

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="blue"
+      color="rgb(57, 65, 41)"
       size={props.size}
       animate={true}
     />
